38c98f4016d5e3984aa1966dbe87d08503e3d36d
Almaz Balbaev <kujitengu@gmail.com>
2024-09-30 14:03:06 +0600

    created user logic

---
+++ src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import {SequelizeModule} from "@nestjs/sequelize";
+import {User} from "./users.models";
+import {Role} from "../roles/roles.models";
+import {UserRoles} from "../user-roles/user-roles.models";
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [
+    SequelizeModule.forFeature([User, Role, UserRoles])
+  ]
+})
+export class UsersModule {}
